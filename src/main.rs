96e4b9226462ca81a25aa01ac14ac3fb4b73ec51
Akatsuki Levi <akatsukilevi@yahoo.co.jp>
2024-12-11 10:51:07 -0300

    fix: Renamed project to `micro_http`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::io::Error;
 
-use mocha_manager::{cli::*, config::init_config, web_server};
+use micro_http::{cli::*, config::init_config, web_server};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
